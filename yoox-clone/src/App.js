5f30b81ef0ea34c0755ae6108378b750dce8fa07
abhijitnr <abhijitbiswas8859@gmail.com>
2022-08-25 19:15:21 +0530

    Men page & Footer added day-3

--- yoox-clone/src/App.js
+++ yoox-clone/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 import Allroutes from './Routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
+      {/* Navbar added */}
       <Navbar />
+      {/* Allroutes is added */}
       <Allroutes />
+      {/* Footer added */}
+      <Footer />
     </div>
   );
 }
--- yoox-clone/src/Routes/AllRoutes.jsx
+++ yoox-clone/src/Routes/AllRoutes.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import MenPage from "../Components/MenPage";
 import WomenPage from "../Components/WomenPage";
 
 const Allroutes = () => {
     return (
         <Routes>
             <Route path="/" element={<WomenPage />} />
-            <Route path="/men" element={<h1>Men Page</h1>} />
+            <Route path="/men" element={<MenPage />} />
             <Route path="/kids" element={<h1>Kids Page</h1>} />
             <Route path="/design+art" element={<h1>Design + Art Page</h1>} />
         </Routes>
